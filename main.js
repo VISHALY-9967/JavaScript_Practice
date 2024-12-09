738a6aee0e8ffed7b434163929f3bb1458d36415
Vishal_Yadav <vishalyadav91665@gmail.com>
2024-12-09 21:27:59 +0530

    Block space & Data types

--- main.js
+++ main.js
@@ -1,5 +1,46 @@
 // It is use to print the message
 console.log("Practice JS");
-console.log("with Vishal");
 console.log("Web Development Begins");
 
+// Variables in JS
+// String number null undefined bollean
+const fullName = "Vishal Yadav" ; 
+num = 45
+value= null;
+val1 = undefined;
+isfollow = true;
+notfollow = false;
+console.log(name);
+console.log(num);
+console.log(value);
+console.log(val1);
+console.log(notfollow);
+console.log(isfollow);
+
+// Variables Rules
+var fullname = "Vimal kumar";
+Fullname = "Rahul kumar";
+console.log(fullname);
+// Data type of variables
+console.log(typeof(fullname));
+console.log(Fullname);
+
+// let const var
+var name = "vikas Yadav" ; 
+let fName = "Vishal" ; 
+const lName = "Yadav" ; 
+
+const PI = 3.14;
+console.log(PI);
+console.log(typeof(PI));
+
+// Bloack Space Variable
+{
+    let a = 5;
+    console.log(a);
+}
+{
+    let a = 10;
+    console.log(a);
+    console.log(typeof(a));
+}

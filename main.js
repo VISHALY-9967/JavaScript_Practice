07908834f678da8522b8c607eacfe00a6af4db71
Vishal_Yadav <vishalyadav91665@gmail.com>
2024-12-09 21:48:56 +0530

    Objects in JS

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ console.log("Web Development Begins");
 
 // Variables in JS
 // String number null undefined bollean
-const fullName = "Vishal Yadav" ; 
-num = 45
-value= null;
+const fullName = "Vishal Yadav";
+num = 45;
+value = null;
 val1 = undefined;
 isfollow = true;
 notfollow = false;
@@ -22,25 +22,57 @@ var fullname = "Vimal kumar";
 Fullname = "Rahul kumar";
 console.log(fullname);
 // Data type of variables
-console.log(typeof(fullname));
+console.log(typeof fullname);
 console.log(Fullname);
 
 // let const var
-var name = "vikas Yadav" ; 
-let fName = "Vishal" ; 
-const lName = "Yadav" ; 
+var name = "vikas Yadav";
+let fName = "Vishal";
+const lName = "Yadav";
 
 const PI = 3.14;
 console.log(PI);
-console.log(typeof(PI));
+console.log(typeof PI);
 
 // Bloack Space Variable
 {
-    let a = 5;
-    console.log(a);
+  let a = 5;
+  console.log(a);
 }
 {
-    let a = 10;
-    console.log(a);
-    console.log(typeof(a));
+  let a = 10;
+  console.log(a);
+  console.log(typeof a);
 }
+
+// object-> Non-primitive data types
+const Student = {
+  fullName: "Raju",
+  age: 18,
+  city: "Delhi",
+};
+
+console.log(Student);
+console.log(typeof(Student));
+
+// Store the details in picture in form of object
+const product ={
+title:"Parker Pen",
+rating:4,
+offer:5,
+price:280,
+};
+
+console.log(product);
+console.log(typeof(product));
+
+// Store the details in picture in form of object
+const profile ={
+    profileName:"Vishal yadav",
+    post:195,
+    isFollow:false,
+    followers:12000,
+    following:1500,
+}
+console.log(profile);
+console.log(typeof(profile));

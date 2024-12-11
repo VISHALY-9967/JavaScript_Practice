49cd6df9bd2e5c5d03d763dee2d581d0ee2f4560
Vishal_Yadav <vishalyadav91665@gmail.com>
2024-12-11 22:05:19 +0530

    IF condition

--- main.js
+++ main.js
@@ -102,3 +102,16 @@ console.log(b);
 
 console.log(a===b);
 console.log(a !== b);
+
+// Conditional comparison operator
+// IF condition
+let mode = "dark";
+let color;
+if(mode ==="dark"){
+  color = "black";
+};
+if(mode === "light"){
+  color = "white";
+}
+
+console.log(color);
